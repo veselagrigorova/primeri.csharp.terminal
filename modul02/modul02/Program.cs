2723e75677eb89e591cf68b986828732f71b9147
весела <adresza@abv.bg>
2015-11-01 20:32:19 +0200

    Модул02.Добавяне на коментари

---
+++ modul02/modul02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+		}
+		//дефиниране на променливи
+
+
+		//събиране на числа
+
+		//писане в конзола
+	}
+}
+
