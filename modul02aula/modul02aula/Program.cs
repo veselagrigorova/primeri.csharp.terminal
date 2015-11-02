d822f236d309a943aca02e59e5cf1bf5d4429867
весела <adresza@abv.bg>
2015-11-02 20:37:17 +0200

    Модул 02.Добавяне на коментари.Видеоурок2

---
+++ modul02aula/modul02aula/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace modul02aula
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+			//Дефиниране на променливи
+
+			//Събиране на числа
+
+			//Писане в конзола
+		}
+	}
+}
