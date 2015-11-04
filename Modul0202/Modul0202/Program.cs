b8f61217dca9fecc8e2edaf7748668d6ef1c0fcd
весела <adresza@abv.bg>
2015-11-04 21:53:52 +0200

    Модул02.Основни оператори за целочислени типове

--- Modul0202/Modul0202/Program.cs
+++ Modul0202/Modul0202/Program.cs
@@ -17,7 +17,7 @@ namespace Modul0202
 
 			//Събиране на числа
 
-			a = 5;
+			a = 6;
 			b = 10;
 			sum = a + b;
 
@@ -27,6 +27,28 @@ namespace Modul0202
 			Console.Write (b);
 			Console.Write (" = ");
 			Console.WriteLine (sum);
+
+			//Допълнителни оператори
+			Console.WriteLine ("\nИзползване на +=");
+			a += b;
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\nИзползване на -=");
+			a -= b;
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\nИзползване на *=");
+			a *= b;
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("\nИзползване на ++");
+			a++;
+			Console.WriteLine ( a );
+
+			Console.WriteLine ("Стандартно делене: " + ( 7 / 3).ToString ());
+			Console.WriteLine ("Стандартно делене: " + ( 7 % 3).ToString ());
+
+
 		}
 	}
 }
