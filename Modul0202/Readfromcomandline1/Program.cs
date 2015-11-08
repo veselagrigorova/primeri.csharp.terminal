080026cbd5d9401974a91285e646e8594da94ffe
весела <adresza@abv.bg>
2015-11-08 09:56:28 +0200

    Модул02. Четене от ред

---
+++ Modul0202/Readfromcomandline1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Readfromcomandline1
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//дефиниране на променливи
+			int a = 5, b = 0;
+
+			//въвеждане на входни параметри
+			Console.Write ("Моля въведете а: ");
+
+			b = Convert.ToInt32 (Console.ReadLine ());
+
+			//печат на резултат
+
+			Console.WriteLine ("Резултатът а + b e  " + (a + b ).ToString() + "\n\n\n");
+		}
+	}
+}
