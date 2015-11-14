39d49e5ddf0af9a1398f8dc5d4d366b2007c8ec9
весела <adresza@abv.bg>
2015-11-14 17:13:57 +0200

    Модул3. Работа със switch

---
+++ Modul03/boolexplore2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace boolexplore2
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			int[] _i = new int []{ 4, 3, 2 };
+
+
+			int _ui = 0;
+			string _input = "0";
+			Console.Write ("Моля, въведете индекс");
+			_input = Console.ReadLine ();
+			bool _check01 = int.TryParse (_input, out _ui);
+
+
+
+//			_ui = (_ui <= _i.Length) ? _ui : 3;
+//			_ui = (_ui > 0) ? _ui : 1;
+
+			switch (_ui) {
+			case 1:
+				{
+					Console.Write ("Първи елемент от масива: ");
+					Console.WriteLine (_i [_ui-1]);
+					break;
+				}
+			case 2:
+				{
+					Console.Write ("Втори елемент от масива: ");
+					Console.WriteLine (_i [_ui-1]);
+					break;
+				}
+
+			case 3:
+				{
+					Console.Write ("Трети елемент от масива: ");
+					Console.WriteLine (_i [_ui-1]);
+					break;
+				}
+					
+			default:
+				
+				{
+					Console.Write ("В масива няма стойност за този индекс.\nОпитайте с индекс между 1 и 3 \n\n");
+					break;
+				}
+
+			}
+		}
+	}
+}
+
+
+
+
+
+	
