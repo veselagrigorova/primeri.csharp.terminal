4ec9ada519911a9009306fded7c2234eaafd7de6
весела <adresza@abv.bg>
2015-11-14 19:55:06 +0200

    Модул04Цикъл.For

---
+++ Modul04/Modul04/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Modul04
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			for (int i = 6; i > 0; i--)
+			{
+				Console.WriteLine (i.ToString ());
+				{
+					//код на цикъла
+				}
+			}
+		}
+	}
+}
+
+
+
