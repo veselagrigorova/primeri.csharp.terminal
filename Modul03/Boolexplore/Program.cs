faaa82deb1a2e77f8843114dc5421edf84bf2c91
весела <adresza@abv.bg>
2015-11-14 12:51:31 +0200

    Модул03. Работа с булеви променливи. If.Else

--- Modul03/Boolexplore/Program.cs
+++ Modul03/Boolexplore/Program.cs
@@ -14,22 +14,67 @@ namespace Boolexplore
 			int[] _i = new int []{ 4, 3, 2 };
 			int _ui = Convert.ToInt32 (Console.ReadLine ());
 
-			bool _check01 = _ui <= _i.Length;
-			bool _check02 = _ui > 0;
+//			bool _check01 = _ui <= _i.Length;
+//			bool _check02 = _ui > 0;
+//
+//			bool _exp01 = _check01 && _check02; //логическо и
+//			bool _exp02 = _check01 || _check02; //логическо или
+//			bool _exp03 = !_check01; //логическо отрицание
+//
+//
+//
+//
+//			Console.WriteLine ("Логическо И: " + _exp01.ToString());
+//			Console.WriteLine ("Логическо ИЛИ: " + _exp02.ToString());
+//			Console.WriteLine ("Отрицание на: " + _check01.ToString () + ":" + _exp03);
 
-			bool _exp01 = _check01 && _check02; //логическо и
-			bool _exp02 = _check01 || _check02; //логическо или
-			bool _exp03 = !_check01; //логическо отрицание
+			if (_ui > 0 && _ui <= _i.Length) {
+				Console.WriteLine (_i [_ui - 1]);
+			} else {
+				if (_ui > _i.Length)
+				{
+					Console.WriteLine ("Индексът е прекалено голям\n\n\n");
+				}
+				if (_ui <= 0)
+				{
+					Console.WriteLine ("Индексът е прекалено малък\n\n\n");
+				}
 
-
-
-
-			Console.WriteLine ("Логическо И: " + _exp01.ToString());
-			Console.WriteLine ("Логическо ИЛИ: " + _exp02.ToString());
-			Console.WriteLine ("Отрицание на: " + _check01.ToString () + ":" + _exp03);
-
-
-
+				
+			}
 		}
+
 	}
 }
+
+	
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+					
+
+
+
+
+
+
