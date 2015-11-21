b081540ecd97020356eb76f18c536a5bd9a9bb10
весела <adresza@abv.bg>
2015-11-21 12:25:49 +0200

    Модул04.Do.While

---
+++ Modul04/listesplore/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace listesplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//дефиниции
+
+			string _userinput = "";
+
+			//потребителски вход
+			 do{
+				Console.Write ("? ");
+
+				_userinput = Console.ReadLine();
+
+			} while (_userinput !="exit");
+		}
+
+	}
+}
+
---
+++ Modul04/listtexplore/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace listtexplore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+
+		{
+			// дефиниции
+			string _userinput = "";
+			//лист
+
+
+			//потребителски вход
+			do{
+				Console.Write ("? ");
+				_userinput=Console.ReadLine ();
+				//добавяне на стойности
+
+				//преглед на листа
+
+				//размер на листа
+
+			}while(_userinput !="exit");
+
+		}
+	}
+}
+ 
+
+
+
+	
+
+
+	
+
+
+
