ce36c6da5dcd8c47f9c5781d63321a24f3ec1c73
весела <adresza@abv.bg>
2015-11-27 22:23:15 +0200

    Модул05.Работа с класове

---
+++ modul05/ClassExplore/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ClassExplore
+{
+	public class person
+	{
+		public string simpleString = "Тестова променлива от клас";
+
+		public person ()
+		{
+			simpleString = "Промяна на променлива при инициализиране";
+		}
+			
+	}
+	class MainClass
+	{
+		
+		public static void Main (string[] args)
+
+		{
+
+			//Дефиниране на клас
+			person _person = new person();
+
+			//Достъпване на клас
+
+			Console.WriteLine ( _person.simpleString );
+		}
+	}
+}
