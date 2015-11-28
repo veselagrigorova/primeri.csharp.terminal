170c1efbaaec7d4abf320f151caca4eea12ea65f
весела <adresza@abv.bg>
2015-11-28 16:02:49 +0200

    Модул05. Дефиниране на променливи.Hidden.Private.

---
+++ modul05/ClassExplorer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ClassExplorer
+{
+	public class person
+	{
+		public string simpleString = "Тестова променлива от клас";
+		public string user = "";
+
+		private string hiddenString = "";
+	
+		public person (string _ini)
+		{
+
+			if (_ini == "Password1") hiddenString = "Потребител 1";
+			if (_ini == "Password2") hiddenString = "Потребител 2";
+
+			user = hiddenString;
+
+			if (hiddenString.Length > 0)
+				
+				simpleString = "Добре дошли отново, " +user+ "!";
+			else
+				simpleString = "Вие нямате достъп до тази програма.";
+		}
+	}
+	class MainClassx
+	{
+		public static void Main (string[] args)
+		{
+			Console.Write ("Парола: "); string  _user = Console.ReadLine ();
+			//Дефиниране на клас
+			person _person = new person( _user);
+			//Достъпване на клас
+				Console.WriteLine ( _person.simpleString);
+		}
+	}
+}
